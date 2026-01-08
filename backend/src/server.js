3854c66675fdb48bc5ec63da22e1aae21a18c14d
N Jayant Rao <njayantrao@gmail.com>
2026-01-08 17:55:31 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app= express()
+
+const port=ENV.PORT || 3000
+
+app.get("/",(req,res)=>{
+    res.status(200).json({msg:"Server up n running..."})
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}...`);
+    console.log(`${ENV.DB_URL}`)
+})
