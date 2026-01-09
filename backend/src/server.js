32b63077f631cd5db5dac150fa3686ef7d385e2a
N Jayant Rao <njayantrao@gmail.com>
2026-01-09 12:03:39 +0530

    added clerk setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
-
+import { db } from "./lib/db.js";
 const app= express()
 
 const port=ENV.PORT || 3000
@@ -11,5 +11,4 @@ app.get("/",(req,res)=>{
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}...`);
-    console.log(`${ENV.DB_URL}`)
 })
