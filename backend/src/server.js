7841f8933aae0c4e36098bff21c008c8e0424b41
N Jayant Rao <njayantrao@gmail.com>
2026-01-14 22:54:11 +0530

    fixed cors errors 1.1

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const app= express()
 app.use(express.json())
 app.use(cors({
     origin:[ENV.FRONTEND_URL,"http://localhost:5173",
-      "http://127.0.0.1:5173",],
+      "http://127.0.0.1:5173","https://code-arena-psi.vercel.app"],
       credentials:true,
        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
